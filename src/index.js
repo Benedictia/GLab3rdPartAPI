a3c2b377df8e30655de033a98b9ede7a304a58e4
Benedictia <179542386+Benedictia@users.noreply.github.com>
2024-11-27 19:03:54 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';  
 import App from './App';  
 
-// Render the App component inside the 'root' div in index.html
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
